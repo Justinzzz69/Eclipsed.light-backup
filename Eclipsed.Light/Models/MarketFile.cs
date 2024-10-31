1676b4eab4c4be771b756223aa0632bad84b0c34
cumback <100005334+Justinzzz69@users.noreply.github.com>
2024-10-31 17:18:55 +0100

    Add files via upload

---
+++ Eclipsed.Light/Models/MarketFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace honeypot.Models
+{
+    public class MarketFile
+    {
+        public string Name { get; set; }
+        public string Hash { get; set; }
+    }
+}
